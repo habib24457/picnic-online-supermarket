eab0f22675e3a73534f37ae3329aa7677593dcde
Habibur Rahman <rahmanhabibur24457@gmail.com>
2021-03-31 01:38:47 +0200

    basic codes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Home page</h1>
+     <Button>Home</Button>
+    </div>
+  );
+}
+
+export default App;
